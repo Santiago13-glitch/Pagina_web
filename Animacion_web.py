e1f3696ff514ce27a56489d72904d135f071e588
Santiago13-glitch <karinalandi@gmail.com>
2024-07-30 19:44:24 -0500

    Create Animacion_web.py

---
+++ Animacion_web.py
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dependencia tecnológica</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Dependencia tecnológica</h1>
+    <img src="https://image.slidesharecdn.com/dependenciatecnolgica-230725235415-398aae4d/85/Dependencia-Tecnologica-pptx-2-320.jpg"width="600px" alt="Image 1">
+    <h2>Una lista de las dependencias tecnológicas más comunes:</h2>
+             <ul>
+                   <li>Redes sociales y mensajeros: Las personas pueden pasar demasiado tiempo en las redes sociales y los mensajeros, reduciendo el tiempo que pasan en el mundo real. Además, esto les lleva a depender de actualizaciones constantes de noticias y mensajes.</li>
+                   <li>Teléfonos inteligentes y tabletas: Los teléfonos inteligentes y las tabletas pueden convertirse en la principal forma de comunicarse con el mundo, y la gente puede volverse dependiente de comprobar constantemente las notificaciones, retuitear y navegar por las redes sociales y los mensajeros.</li>
+                   <li>Juegos y entretenimiento en línea: Los juegos y el ocio en línea pueden crear adicción y llevar a la gente a pasar demasiado tiempo en el mundo virtual en lugar de en la vida real.</li>
+                   <li>Compras por Internet y compras en línea: Las personas pueden volverse adictas a las compras en línea, sobre todo si las utilizan para satisfacer necesidades emocionales.</li>
+                   <li>Servicios de vídeo y streaming en línea: las personas pueden pasar demasiado tiempo en sitios web de alojamiento de vídeos y servicios de streaming, lo que puede provocar una pérdida de tiempo y un menor interés por el mundo real.</li>
+                   <li>Programas y aplicaciones informáticas: Las personas pueden volverse adictas a determinados programas y aplicaciones que pueden distraerlas de su trabajo o sus estudios.</li>
+                   <li>Tecnologías de Inteligencia Artificial: las personas pueden volverse dependientes del uso de tecnologías de inteligencia artificial, como los asistentes de voz, lo que puede provocar la degradación de las capacidades de comunicación y toma de decisiones.</li>
+                   <li>Dispositivos domésticos inteligentes: las personas pueden volverse dependientes del uso de dispositivos domésticos inteligentes, como los asistentes de voz, lo que puede provocar la degradación de las capacidades de comunicación y toma de decisiones.</li>
+                   <li>Tecnologías de realidad virtual y aumentada: las personas pueden volverse dependientes del uso de las tecnologías de realidad virtual y aumentada, lo que puede provocar una disminución del interés por el mundo real y la degradación de las habilidades comunicativas.</li>
+            </ul>
+            <img src="https://revistavive.com/wp-content/uploads/2020/01/NOMOFOBIA2.png"width="300px" alt="Image 1">
+    <h3>¿Cómo combatir la dependencia tecnológica?</h3>
+            <ul>
+                <li>Establecer límites sobre el tiempo que pasas en línea/en tus dispositivos;</li>
+                <li>Diversifique sus intereses y actividades más allá del uso de la tecnología;</li>
+                <li>Busca nuevas formas de conectar con el mundo que te rodea para reducir tu dependencia de las redes sociales y la mensajería;</li>
+                <li>Intenta limitar el tiempo que pasas en el trabajo con el ordenador y tomar descansos (si es posible). Esto reducirá la tensión en tus ojos y te ayudará a despejar la mente antes de la siguiente sesión de trabajo.</li>
+                <li>Si es posible, programe un tiempo libre de tecnología para relajarse y desconectar.</li>
+            </ul>
+    <h4>⭐⭐⭐⭐⭐⭐⭐⭐¡LA TECNOLOGIA NO DEBE SUSTITUIR TU VIDA!⭐⭐⭐⭐⭐⭐⭐⭐</h4>
+
+</body>
+</html>
+
+body {
+    font-family: Arial,  sans-serif; /* Familia de fuentes */
+    font-size: 16px; /* El tamaño de la fuente principal en el tag <p></p> */
+    background-color: #6a89b8; /* El color del fondo de la página web */
+    color: #1e3442; /* El color del texto*/ 
+}
+h1 {
+    color: #120536; /* El color de la cabecera */
+    font-size: 50px; /* El tamaño de la fuente de la cabecera */
+    font-family: Georgia, serif; /* La familia tipográfica utilizada en la cabecera */
+}
+
+img {
+    border-radius: 30%;
+}
+
+@keyframes color-change {
+    0% {
+      color: rgb(182, 45, 102);
+    }
+  
+    50% {
+      color: rgb(69, 22, 107);
+    }
+  
+    100% {
+      color: rgb(182, 45, 102);
+    }
+
+}
+
+h1{
+    animation: color-change 5s infinite;
+}
+
+h2 {
+    animation: color-change 3s infinite;
+}
+
+h3 {
+    animation: color-change 3s infinite;
+}
+
+h4 {
+    animation: color-change 3s infinite;
+}
